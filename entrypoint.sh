7de08c6f8ee52b5603b1255903633e6da58006c7
funnymdzz <funnymdzz@gmail.com>
2019-10-27 18:22:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /v2raybin
-echo $PORT
-neofetch
+
 ./ngrok authtoken BDZIXnhJt2HNWLXyQ5PM_qCaBq0W2sNFcCa0rfTZd -log stdout --log-level debug 
 ./ngrok tcp 8080 -log stdout --log-level debug &
 ./v2ray &
