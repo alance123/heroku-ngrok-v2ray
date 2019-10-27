5be6b121764a6f9833934d60c1571c89dac5aa33
funnymdzz <funnymdzz@gmail.com>
2019-10-27 16:29:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 cd /v2raybin
 echo $PORT
 sed -i "s/PORT/8080/g" config.json
-./v2ray &
-./ngrok tcp authtoken 7GTsuP7sewoVemaBZrbZK_6C9w29ynA6r6TaSu6F2p5 -log stdout --log-level debug 8080
+./ngrok tcp -authtoken 7GTsuP7sewoVemaBZrbZK_6C9w29ynA6r6TaSu6F2p5 -log stdout --log-level debug 8080
+./v2ray
