d4c23703f56687fb4cbdffecc4811b2070b811f3
funnymdzz <funnymdzz@gmail.com>
2019-10-27 18:25:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /v2raybin
 
-./ngrok authtoken BDZIXnhJt2HNWLXyQ5PM_qCaBq0W2sNFcCa0rfTZd -log stdout --log-level debug 
-./ngrok tcp 8080 -log stdout --log-level debug &
-./v2ray &
+./ngrok authtoken BDZIXnhJt2HNWLXyQ5PM_qCaBq0W2sNFcCa0rfTZd
+./ngrok tcp 8080 -log stdout --log-level debug
